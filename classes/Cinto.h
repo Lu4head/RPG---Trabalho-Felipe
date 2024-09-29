655b3231f7e91baf8e0b11b6c8d86034bdc2b3ca
GollinoGui <131721946+GollinoGui@users.noreply.github.com>
2024-09-29 12:55:01 -0300

    NOMES

--- classes/Cinto.h
+++ classes/Cinto.h
@@ -1,3 +1,7 @@
+//Luan Emanuel R. Argentato RA: 2184611
+//Gustavo duzzi             RA: 2214047
+//Guilherme Santos Gollino  RA: 2226090
+
 #pragma once  // pragma once para não espelhar os arquivos importados
 
 #include "Item.h" // Importação da classe item que vai ser usado para interagir o cinto, o cinto é uma lista de itens...
