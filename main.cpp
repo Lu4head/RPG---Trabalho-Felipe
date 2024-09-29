def0127dfc876c144e8da340c06e37884bb336cd
GollinoGui <131721946+GollinoGui@users.noreply.github.com>
2024-09-29 13:01:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+//Luan Emanuel R. Argentato RA: 2184611
+//Gustavo duzzi             RA: 2214047
+//Guilherme Santos Gollino  RA: 2226090
 
 #include ".\classes\Mapa.h"
 #include ".\classes\Cinto.h"
